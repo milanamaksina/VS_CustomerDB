99d4bff94bc97219f4b02c6ed3570446a2940b60
Milana Maksina <m_maksinaa@mail.ru>
2022-08-21 18:32:49 +0300

    feat: add dbo

---
+++ VS_CustomerDB/dbo/Tables/Addresses.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Addresses] (
+    [AddressId]    INT            IDENTITY (1, 1) NOT NULL,
+    [CustomerId]   INT            NOT NULL,
+    [Line]         NVARCHAR (100) NOT NULL,
+    [Line2]        NVARCHAR (100) NULL,
+    [AddressType]  NVARCHAR (8)   NOT NULL,
+    [City]         NVARCHAR (50)  NOT NULL,
+    [PostalCode]   NVARCHAR (6)   NOT NULL,
+    [AddressState] NVARCHAR (20)  NOT NULL,
+    [Country]      NVARCHAR (14)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([AddressId] ASC),
+    CHECK ([AddressType]='Billing' OR [AddressType]='Shipping'),
+    CHECK ([Country]='canada' OR [Country]='united States' OR [Country]='Canada' OR [Country]='United States'),
+    FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId])
+);
+
---
+++ VS_CustomerDB/dbo/Tables/Customers.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustomerId]           INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]            NVARCHAR (50) NULL,
+    [LastName]             NVARCHAR (50) NOT NULL,
+    [PhoneNumber]          NVARCHAR (20) NULL,
+    [Email]                NVARCHAR (50) NULL,
+    [TotalPurchasesAmount] MONEY         NULL,
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC),
+    CHECK ([Email] like '%[a-zA-Z0-9][@][a-zA-Z0-9]%[.][a-zA-Z0-9]%'),
+    CHECK ([PhoneNumber] like '+[1-9]'+replicate('[0-9]',(14)) OR [PhoneNumber] like '[1-9]'+replicate('[0-9]',(14))),
+    CHECK ([TotalPurchasesAmount]>=(0))
+);
+
